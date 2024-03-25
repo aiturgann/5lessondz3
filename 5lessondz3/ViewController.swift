6ccfe5298bd9eeea2050131315850d868f1b59b2
aiturann <aiturgannnnnn@gmail.com>
2024-03-26 02:13:28 +0600

    Initial Commit

---
+++ 5lessondz3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5lessondz3
+//
+//  Created by Aiturgan Kurmanbekova on 26/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
